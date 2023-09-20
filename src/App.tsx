daa53ef266ca9a705089ac279bf4d3ee96aeb7af
Bilyana Mancheva <bilyana.mancheva1991@gmail.com>
2023-09-20 11:31:41 +0300

    chore: add initial setup + one test component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarTest
+  from './components/navbar';
+const App = (): JSX.Element => {
+
+  return (
+    <>
+      <NavBarTest />
+    </>
+  )
+};
+
+export default App;
